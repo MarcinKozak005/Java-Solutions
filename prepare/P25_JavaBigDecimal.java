4b456b6a1abe000efbb0e69d343b5be43d8eeee7
Marcin Kozak <marcinkozak005@gmail.com>
2022-09-30 19:29:27 +0200

    Add Prepare/Java/BigNumber

---
+++ prepare/P25_JavaBigDecimal.java
@@ -0,0 +1,26 @@
+public class P25_JavaBigDecimal {
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+        List<String> list = new ArrayList<>();
+        for (String e : s) if(e!=null) list.add(e);
+        list.sort(new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                return new BigDecimal(o2).compareTo(new BigDecimal(o1));
+            }
+        });
+        s = list.toArray(new String[0]);
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
---
+++ prepare/P26_JavaPrimalityTest.java
@@ -0,0 +1,10 @@
+public class P26_JavaPrimalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        System.out.println(
+            (new BigInteger(n).isProbablePrime(1))?"prime":"not prime");
+        bufferedReader.close();
+    }
+}
