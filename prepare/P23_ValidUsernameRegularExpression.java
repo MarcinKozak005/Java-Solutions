48bdc44790bdee958784e467e69903ab4526a92e
Marcin Kozak <marcinkozak005@gmail.com>
2022-09-30 16:36:25 +0200

    Add Prepare/Java/Strings

---
+++ prepare/P23_ValidUsernameRegularExpression.java
@@ -0,0 +1,6 @@
+public class P23_ValidUsernameRegularExpression {
+    class UsernameValidator {
+        public static final String regularExpression = "[a-zA-Z][0-9a-zA-Z_]{7,29}";
+    }
+    
+}
