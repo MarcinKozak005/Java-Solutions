60389c6947ee395098836024abc77718635c958c
Marcin Kozak <marcinkozak005@gmail.com>
2023-01-26 23:20:09 +0100

    Add 1WeekPrepKit:Day1

---
+++ java/P2_1WeekPreparationKit/D01P02_MinMaxSum.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    // My code start
+    // change in the original code all int to longs
+    public static void miniMaxSum(List<Long> arr) {
+        long min = arr.get(0);
+        long max = arr.get(0);
+        long sum = 0;
+        for(Long e: arr){
+            min = (e < min) ? e : min;
+            max = (e > max) ? e : max;
+            sum += e;
+        }
+        System.out.println((sum-max) + " " + (sum-min));
+    }
+    // My code end
+
+}
+
+public class D02P01_MinMaxSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
---
+++ java/P2_1WeekPreparationKit/D01P02_TimeConversion.java
@@ -0,0 +1,16 @@
+public class D01P02_TimeConversion {
+    class Result {
+        //My code start
+        public static String timeConversion(String s) {
+            int hours = Integer.parseInt(s.substring(0,2));
+            if(s.endsWith("PM")){
+                hours = (hours != 12) ? (hours+12) : 12;
+            } else {
+                hours = (hours != 12) ? hours : 0;
+            }
+            return String.format("%02d%s",hours,s.substring(2,s.length()-2));
+        }
+        // My code end
+    }
+    
+}
